060ac42d7530dc9a97539a5746a33977012297c9
Mauricio Lores <maurolores1992@gmail.com>
2023-11-25 18:02:58 -0300

    boda

---
+++ out/_next/static/B8lD_39bRCGjuQl9E5ZiE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/954-bbb539d1eef54fb5.js","static/css/ae4ed9c503fd1e33.css","static/chunks/pages/index-c100154de2ee1bda.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
